c0675dc92fc7e0da6877ed42cd1535e585402f6a
Onur <onuraksar96@gmail.com>
2025-10-05 20:50:18 +0200

    Services

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,25 +1,29 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { Todo } from '../model/todo.type';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class TodoService {
-
-  todoItems: Array<Todo> = [
-    {
-      title: "Groceries",
-      id: 0,
-      userId: 1,
-      completed: false
-    },
-    {
-      title: "Car Wash",
-      id: 1,
-      userId: 1,
-      completed: false
-    }
-  ]
-  constructor() { }
+  http = inject(HttpClient)
+  getTodosFromApi() {
+    const url = "https://68e190c38943bf6bb3c47c14.mockapi.io/todos/items";
+    return this.http.get<Array<Todo>>(url)
+  }
+  // todoItems: Array<Todo> = [
+  //   {
+  //     title: "Groceries",
+  //     id: 0,
+  //     userId: 1,
+  //     completed: false
+  //   },
+  //   {
+  //     title: "Car Wash",
+  //     id: 1,
+  //     userId: 1,
+  //     completed: false
+  //   }
+  // ]
 }
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { TodoService } from '../services/todo.service';
 import { Todo } from '../model/todo.type';
+import { catchError } from 'rxjs';
 
 @Component({
   selector: 'app-todos',
@@ -11,12 +12,34 @@ import { Todo } from '../model/todo.type';
 export class TodosComponent implements OnInit {
   todoService = inject(TodoService);
   todoItems = signal<Array<Todo>>([]);
+  todoItemText = signal<string>("");
 
   ngOnInit(): void {
     console.log('runs when the component is initiated');
-    console.log(this.todoService.todoItems);
-    this.todoItems.set((this.todoService.todoItems))
+    this.todoService.getTodosFromApi().pipe(
+      catchError((err) => {
+        console.log(err)
+        throw err;
+      })
+    ).subscribe((todos) => {
+      this.todoItems.set(todos)
+    } )
 
+
+    // console.log(this.todoService.todoItems);
+    // this.todoItems.set((this.todoService.todoItems))
+  }
+
+  onChange(event: any) {
+    console.log("event:", event?.target?.value)
+    this.todoItemText.update(text => text = event?.target?.value)
+  }
+  onSubmit() {
+    // todo: send post request here and after that call get request again
+    console.log('submitting!')
+    
+
+    this.todoItemText.set("")
   }
 
 }
